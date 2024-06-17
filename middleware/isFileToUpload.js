eb19691aa190586f7da7fbfb5483d76ec8faa3ed
jerome <151376824+Jerome49-3@users.noreply.github.com>
2024-06-17 14:13:22 +0200

    add default avatar

--- middleware/isFileToUpload.js
+++ middleware/isFileToUpload.js
@@ -1,66 +1,48 @@
 const cloudinary = require("cloudinary").v2;
+const fileUpload = require("express-fileupload");
 const convertToBase64 = require("../utils/convertToBase64");
-
 const isFileToUpload = async (req, res, next) => {
-  // const options = {
-  //   use_filename: true,
-  //   unique_filename: false,
-  //   overwrite: true,
-  // };
   try {
-    //**** verifier la précense de req.files.pîctures ****//
-    // console.log("req.files.pictures before if:", "\n", req.files);
-    //**** si req.files.pîctures est différent de null ou de 0 ****//
-    if (req.files !== null || req.files.pictures !== 0) {
-      //**** stocker req.files.pîctures ds une variable ****//
+    console.log("req.files.pictures before if:", "\n", req.files);
+    if (req.files !== undefined || req.files.pictures !== 0) {
       const pictureToUpload = req.files.pictures;
       // console.log("pictureToUpload:", pictureToUpload);
-      //**** stocker la vérification de pictureToUpload dans une variable const 'arrayPictures' pour savoir si c'est un tableau ****//
       const arrayPictures = Array.isArray(pictureToUpload);
       // console.log("arrayPictures:", arrayPictures);
-      //**** si 'arrayPictures' n'est pas un tableau ****//
       if (arrayPictures === false) {
-        //**** on convertit le buffer (données en language binaire, temporaire pour être utilisé) de l'image en base64 pour etre compris par cloudinary ****//
         const result = await cloudinary.uploader.upload(
           convertToBase64(pictureToUpload)
         );
         // console.log("resultnotPromise:", result);
-        //**** je stocke les données de la conversion en base64 du buffer de l'image dans req ****//
         req.uploadOneFile = result;
         // console.log("coucouIFResult:", req.uploadOneFile);
-        //**** on quitte le middleware et passe à la suite du code ****//
         return next();
-        //**** si arrayPictures est un tableau ****//
       } else if (arrayPictures === true) {
+        console.log(
+          "req.files.pictures after else if:",
+          "\n",
+          req.files.pictures
+        );
         // console.log(
         //   "req.files.pictures after else if:",
         //   "\n",
         //   req.files.pictures
         // );
-        //**** je stocke req.files.pictures dans une constante ****//
         const picUpload = req.files.pictures;
-        // console.log("picUpload:", picUpload);
-        //**** pour chaque image convertir en base64 et envoyer les envoyer les images à cloudinary ****//
+        // console.log("picturesToUpload:", picUpload);
         const arrayOfPromises = picUpload.map((picture) => {
-          // console.log("picture:", picture);
           return cloudinary.uploader.upload(convertToBase64(picture));
         });
-        //**** attendre le fin de l'upload pour tous les fichiers et les stocker dans une constante ****//
         const result = await Promise.all(arrayOfPromises);
         // console.log("resultPromise:", result);
-        //**** stocker les informations des images dans req ****//
         req.uploadMultiFile = result;
-        //console.log("coucouIFResult:", req.uploadMultiFile);
-        //**** on quitte le middleware et passe à la suite du code ****//
+        // console.log("coucouIFResult:", req.uploadMultiFile);
         return next();
       }
-    }
-    //**** si req.files.pîctures est null ou 0, on retourne une erreur 400 bad request ****//
-    else {
-      return res.status(400).json({ result, message: "bad request" });
+    } else {
+      return res.status(400).json({ message: "bad request" });
     }
   } catch (error) {
-    //**** si le try echoue (erreur server), on retourne une erreur ****//
     console.log("error.message:", "\n", error.message);
   }
 };
